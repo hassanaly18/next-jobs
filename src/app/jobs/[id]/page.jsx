5c9a58c402b4305eee4405e0b61d5a63f7d098c2
Hassan Ali <hassansiddiqui03024053707@gmail.com>
2025-03-13 21:47:31 +0500

    post day 3

--- src/app/jobs/[id]/page.jsx
+++ src/app/jobs/[id]/page.jsx
@@ -7,6 +7,15 @@ import React, { useEffect, useState } from 'react'
 const JobDetails = () => {
     const {id} = useParams();
     const [job, setJob] = useState(null);
+    const [showform, setShowform] = useState(true);
+    const [applicant, setApplicant]= useState(
+        {
+            name: "",
+            email: "",
+            cover_letter: "",
+            developer_id: "18f21b77-9bb9-4aea-89a6-7c7e1cfdb054",
+        },
+    );
 
     useEffect(()=>{
         async function fetchJob(){
@@ -23,24 +32,65 @@ const JobDetails = () => {
         fetchJob();
     }, [id])
 
+    const handleApply= async()=>{
+        if(!applicant.name || !applicant.email || !applicant.cover_letter){
+            alert("All the fields are required..!!")
+            return;
+        }
+        
+        const {error} = await supabase.from("applications").insert([
+            {
+                job_id: id,
+                developer_id: applicant.developer_id,
+                cover_letter: applicant.cover_letter,
+            }
+        ])
+
+        if(error){
+            alert(error.message)
+        }
+        else{
+            alert("The Application is submitted.!")
+        }
+
+        setShowform(false)
+    }
+
     if(!job) return <p>Loading....</p>
     
   return (
     <>
-    <div className='p-6'>
-        <h1 className='text-2xl font-bold'>
+    <div className='max-w-3xl mt-10 mx-auto p-8 bg-gray-900 shadow-md rounded-lg'>
+        <h1 className='text-3xl font-bold text-gray-200'>
             {job.title}
         </h1>
-        <p className='text-gray-700'>
+        <p className='text-gray-700 mt-2'>
             {job.description}
         </p>
-        <p className='text-gray-500'>
+        <p className='text-gray-500 font-medium mt-2'>
             {job.company}
         </p>
 
         <button className='mt-4 bg-green-600 text-white px-4 py-2'>
             Apply Now!
         </button>
+
+        {showform && (
+            <div className='mt-6 p-6 border rounded-lg bg-gray-900'>
+                <h2 className='text-xl font-semibold text-gray-200'>
+                    Apply for this Job Now!
+                </h2>
+
+                <div className='space-y-4 mt-4'>
+                    <input placeholder='Full Name' type="text" name='name' value={applicant.name} className='w-full px-4 py-2 border rounded-md bg-gray-800 focus:ring-2 focus:ring-blue-500'/>
+                    <input type="email" placeholder='Enter your Email' name='email'  value={applicant.email} className='w-full px-4 py-2 border rounded-md bg-gray-800 focus:ring-2 focus:ring-blue-500'/>
+                    <textarea placeholder='Your Cover Letter' name='cover_letter' value={applicant.cover_letter} className='w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500 h-32 resize-none'></textarea>
+                    <button onClick={handleApply} className='w-full bg-blue-500 text-white py-3 rounded-md font-medium hover:bg-blue-800 transition disabled:bg-gray-500'>
+                        Submit Application
+                    </button>
+                </div>
+            </div>
+        )}
     </div>
     </>
   )
--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -11,7 +11,7 @@ const PostJob = () => {
 
     const postJob = async() => {
         if(!title || !description || !company){
-            alert("Fill out all the firlds..!!")
+            alert("Fill out all the fields..!!")
             return;
         }
 
@@ -32,13 +32,13 @@ const PostJob = () => {
 
   return (
     <>
-    <div className='p-6'>
-        <h1 className='text-2xl font-bold mb-4'>Post a Job</h1>
-        <input className='border' type="text" placeholder='Job Title' value={title} onChange={(e)=>setTitle(e.target.value)}/>
-        <textarea className='' placeholder='Job Description' value={description} onChange={(e)=>setDescripton(e.target.value)}/>
-        <input className='border' type="text" placeholder='Company' value={company} onChange={(e)=>setCompany(e.target.value)}/>
+    <div className='p-8 mt-10 max-w-2xl mx-auto shadow-md bg-gray-900 rounded-lg'>
+        <h1 className='text-3xl font-bold mb-4 text-gray-100 mb-6'>Post a Job</h1>
+        <input className='w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500' type="text" placeholder='Job Title' value={title} onChange={(e)=>setTitle(e.target.value)}/>
+        <textarea className='w-full mt-6 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 h-32' placeholder='Job Description' value={description} onChange={(e)=>setDescripton(e.target.value)}/>
+        <input className='w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 mt-2 focus:ring-blue-500' type="text" placeholder='Company' value={company} onChange={(e)=>setCompany(e.target.value)}/>
 
-        <button onClick={postJob}>
+        <button className='w-full mt-4 bg-blue-600 text-white py-3 rounded-md font-medium hover:bg-blue-700 transition disabled:bg-gray-400' onClick={postJob}>
             Post
         </button>
     </div>
