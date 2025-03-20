5a3ea771effa2fe7fcea42d90cf7d9584ad5a471
Hassan Ali <hassansiddiqui03024053707@gmail.com>
2025-03-20 21:27:38 +0500

    supabase-server fix

--- src/utils/supabase-server.js
+++ src/utils/supabase-server.js
@@ -1,7 +1,7 @@
 import {createServerClient} from "@supabase/auth-helpers-nextjs";
 import {cookies} from "next/headers";
 
-export const createSupabaseServerClient()=>{
+export const createSupabaseServerClient = () => {
 
     return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, {cookies});
 }
